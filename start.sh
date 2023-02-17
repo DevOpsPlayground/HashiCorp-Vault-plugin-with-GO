74ac246e6a082b202d8a3b8816065ff037ab83cf
James Sandford <sandford.james@gmail.com>
2023-02-17 09:04:56 +0000

    feat: adding fix for docker-compose

--- start.sh
+++ start.sh
@@ -1,11 +1,18 @@
-	set -o xtrace
+#!/bin/bash
+set -o xtrace
 
-    docker compose down
-	docker compose up -d
-	sleep 5
-	export VAULT_ADDR='http://127.0.0.1:8200'
-    echo $VAULT_ADDR
-	TOKENS=$(vault operator init -format=json -key-shares=1 -key-threshold=1)
-	echo $TOKENS | jq -r '.unseal_keys_b64[0]';
-    vault operator unseal $(echo $TOKENS | jq -r .unseal_keys_b64[0])
-    echo $(echo $TOKENS | jq -r '.root_token')
+docker-compose --version
+if [ $? -eq 0 ]; then
+  docker-compose down
+  docker-compose up -d
+else
+  docker compose down
+  docker compose up -d
+fi
+sleep 5
+export VAULT_ADDR='http://127.0.0.1:8200'
+echo $VAULT_ADDR
+TOKENS=$(vault operator init -format=json -key-shares=1 -key-threshold=1)
+echo $TOKENS | jq -r '.unseal_keys_b64[0]';
+vault operator unseal $(echo $TOKENS | jq -r .unseal_keys_b64[0])
+echo $(echo $TOKENS | jq -r '.root_token')
